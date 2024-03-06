ef84e89620bd3b84340e115a7a64215d4f0087de
James Moseti <moseti.james4@gmail.com>
2024-03-06 11:20:24 +0300

    Bug fixes on Dashboard

--- Dashboard.vb
+++ Dashboard.vb
@@ -3,89 +3,41 @@
         logoPictureBox.BorderStyle = BorderStyle.None
     End Sub
     Private Sub Dashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        pnlfood.Visible = True
-        pnldrinks.Visible = False
-        pnlcart.Visible = False
-        pnlabout.Visible = False
 
     End Sub
 
     Private Sub btnFoods_Click(sender As Object, e As EventArgs) Handles btnFoods.Click
-<<<<<<< HEAD
         pnlOnButtonPosition.Height = btnFoods.Height
         pnlOnButtonPosition.Top = btnFoods.Top
         pnlFoods.Visible = True
         pnlDrinks.Visible = False
-=======
-        pnlfood.Height = btnFoods.Height
-        pnlfood.Top = btnFoods.Top
-        pnlfood.Visible = True
-        pnldrinks.Visible = False
-        pnlcart.Visible = False
-        pnlabout.Visible = False
-        'pnlFoods.Visible = True
-        'pnlDrinks.Visible = False
->>>>>>> 2212dfbf1755d5570f628be0f730dd90b7798aa6
         'pnlMyCart.Visible = False
         'pnlAboutUs.Visible = False
     End Sub
 
     Private Sub btnDrinks_Click(sender As Object, e As EventArgs) Handles btnDrinks.Click
-<<<<<<< HEAD
         pnlOnButtonPosition.Height = btnDrinks.Height
         pnlOnButtonPosition.Top = btnDrinks.Top
         pnlFoods.Visible = False
         pnlDrinks.Visible = True
-=======
-        pnlfood.Height = btnDrinks.Height
-        pnlfood.Top = btnDrinks.Top
-        pnlfood.Visible = False
-        pnldrinks.Visible = True
-        pnlcart.Visible = False
-        pnlabout.Visible = False
-        'pnlFoods.Visible = False
-        'pnlDrinks.Visible = True
->>>>>>> 2212dfbf1755d5570f628be0f730dd90b7798aa6
         'pnlMyCart.Visible = False
         'pnlAboutUs.Visible = False
     End Sub
 
     Private Sub btnMyCart_Click(sender As Object, e As EventArgs) Handles btnMyCart.Click
-<<<<<<< HEAD
         pnlOnButtonPosition.Height = btnMyCart.Height
         pnlOnButtonPosition.Top = btnMyCart.Top
         pnlFoods.Visible = False
         pnlDrinks.Visible = False
-=======
-        pnlfood.Height = btnMyCart.Height
-        pnlfood.Top = btnMyCart.Top
-        pnlfood.Visible = False
-        pnldrinks.Visible = False
-        pnlcart.Visible = True
-        pnlabout.Visible = False
-        'pnlFoods.Visible = False
-        'pnlDrinks.Visible = False
->>>>>>> 2212dfbf1755d5570f628be0f730dd90b7798aa6
         'pnlMyCart.Visible = True
         'pnlAboutUs.Visible = False
     End Sub
 
     Private Sub btnAboutUs_Click(sender As Object, e As EventArgs) Handles btnAboutUs.Click
-<<<<<<< HEAD
         pnlOnButtonPosition.Height = btnAboutUs.Height
         pnlOnButtonPosition.Top = btnAboutUs.Top
         pnlFoods.Visible = False
         pnlDrinks.Visible = False
-=======
-        pnlfood.Height = btnAboutUs.Height
-        pnlfood.Top = btnAboutUs.Top
-        pnlfood.Visible = False
-        pnldrinks.Visible = False
-        pnlcart.Visible = False
-        pnlabout.Visible = True
-        'pnlFoods.Visible = False
-        'pnlDrinks.Visible = False
->>>>>>> 2212dfbf1755d5570f628be0f730dd90b7798aa6
         'pnlMyCart.Visible = False
         'pnlAboutUs.Visible = True
     End Sub
